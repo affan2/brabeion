511784df0276bca89e7b2bf29b0dc4d9694b4f92
Mohammad Abouchama <mohammadabouchama@gmail.com>
2019-10-16 13:37:22 +0400

    Disable addition and editing from the back-end

--- pinax/badges/admin.py
+++ pinax/badges/admin.py
@@ -2,4 +2,13 @@ from django.contrib import admin
 
 from .models import BadgeAward
 
-admin.site.register(BadgeAward)
+
+class BadgeAwardAdmin(admin.ModelAdmin):
+    def has_add_permission(self, request, obj=None):
+        return False
+
+    def get_readonly_fields(self, request, obj=None):
+        return [f.name for f in self.model._meta.fields]
+
+
+admin.site.register(BadgeAward, BadgeAwardAdmin)
